4e177b09cba27813e68237699a900e333e9675f2
bala-amypo <bala@amypo.in>
2025-12-19 15:07:38 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,29 +1,23 @@
-package com.example.demo.service.impl;
-
-import com.example.demo.model.User;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.security.JwtTokenProvider;
-import com.example.demo.service.UserService;
-
-public class UserServiceImpl implements UserService {
-
-    public UserServiceImpl(UserRepository repo, JwtTokenProvider jwt) {}
-
-    @Override
-    public User register(String email, String password, String role) {
-        User u = new User();
-        u.setEmail(email);
-        u.setRole(role);
-        return u;
-    }
-
-    @Override
-    public User login(String email, String password) {
-        throw new RuntimeException("User not found");
-    }
-
-    @Override
-    public User getByEmail(String email) {
-        throw new RuntimeException("User not found");
-    }
-}
+[INFO] -------------------------------------------------------------
+[ERROR] COMPILATION ERROR : 
+[INFO] -------------------------------------------------------------
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/service/impl/VendorServiceImpl.java:[11,8] class UserServiceImpl is public, should be declared in a file named UserServiceImpl.java
+[INFO] 1 error
+[INFO] -------------------------------------------------------------
+[INFO] ------------------------------------------------------------------------
+[INFO] BUILD FAILURE
+[INFO] ------------------------------------------------------------------------
+[INFO] Total time:  10.384 s
+[INFO] Finished at: 2025-12-19T15:06:44Z
+[INFO] ------------------------------------------------------------------------
+[ERROR] Failed to execute goal org.apache.maven.plugins:maven-compiler-plugin:3.13.0:compile (default-compile) on project demo: Compilation failure
+[ERROR] /home/coder/Workspace/demo/src/main/java/com/example/demo/service/impl/VendorServiceImpl.java:[11,8] class UserServiceImpl is public, should be declared in a file named UserServiceImpl.java
+[ERROR] 
+[ERROR] -> [Help 1]
+[ERROR] 
+[ERROR] To see the full stack trace of the errors, re-run Maven with the -e switch.
+[ERROR] Re-run Maven using the -X switch to enable full debug logging.
+[ERROR] 
+[ERROR] For more information about the errors and possible solutions, please read the following articles:
+[ERROR] [Help 1] http://cwiki.apache.org/confluence/display/MAVEN/MojoFailureException
+coder@b648339d6b91:~/Workspace/demo$ 
