223a8639c92afd97b62ed7ef2f9f079c62ab7758
bala-amypo <bala@amypo.in>
2025-12-19 04:32:28 +0000

    User commit

--- src/main/java/com/example/demo/repository/VendorRepository.java
+++ src/main/java/com/example/demo/repository/VendorRepository.java
@@ -1,14 +1,8 @@
 package com.example.demo.repository;
 
-import java.util.Optional;
-
+import com.example.demo.model.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.demo.entity.Vendor;
-
 public interface VendorRepository extends JpaRepository<Vendor, Long> {
-
     boolean existsByName(String name);
-
-    Optional<Vendor> findByName(String name);
 }
--- src/main/java/com/example/demo/service/VendorService.java
+++ src/main/java/com/example/demo/service/VendorService.java
@@ -1,15 +1,12 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Vendor;
 import java.util.List;
-import com.example.demo.entity.Vendor;
 
 public interface VendorService {
-
     Vendor createVendor(Vendor vendor);
-
+    Vendor updateVendor(Long id, Vendor vendor);
     Vendor getVendorById(Long id);
-
     List<Vendor> getAllVendors();
-
     void deactivateVendor(Long id);
 }
