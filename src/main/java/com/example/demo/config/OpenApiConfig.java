f4741b25a171dcd623df60a9df10bc415472af06
bala-amypo <bala@amypo.in>
2025-12-20 16:41:16 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,3 +1,26 @@
 package com.example.demo.config;
 
-public class OpenApiConfig {}
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        Server amypoServer = new Server();
+        amypoServer.setUrl("https://9426.pro604cr.amypo.ai/");
+        amypoServer.setDescription("Amypo Preview Server");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Vendor SLA Performance Tracker API")
+                        .description("API documentation for Vendor SLA Performance Tracker")
+                        .version("1.0.0"))
+                .addServersItem(amypoServer);
+    }
+}
